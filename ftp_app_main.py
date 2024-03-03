9883bf9bc6f18b9bc6a2b5b51a5ce70cfd32d40b
alysis <aldosiswanto369@gmail.com>
2024-03-02 18:21:41 -0800

    Added ftp_app_controller, added threading to main

--- ftp_app_main.py
+++ ftp_app_main.py
@@ -3,56 +3,20 @@ import queue
 import threading
 import time
 
-
-class main_thread():
-    VIEW = ftp_app_view
-
-    def __init__(self):
-        # Create queues
-        self.status_queue = queue.Queue()
-        self.job_queue = queue.Queue()
-
-        # Create threads
-        self.threads_list = []
-        self.view_thread = self.spawn_thread(self.run_view, status_queue=self.status_queue, job_queue=self.job_queue)
-
-
-    def start(self):
-        """ Start all the threads """
-        for thread in self.threads_list:
-            thread.start()
-
-    def stop(self):
-        """ Check for all thread to join """
-
-        for thread in self.threads_list:
-            print(f'waiting for thread: {thread}')
-            thread.join()
-            print('thread stopped')
-
-    def spawn_thread(self, target, **kwargs):
-        """ Spawns a new thread with specified target and append to threads list"""
-        thread = threading.Thread(target=target, kwargs=kwargs)
-        self.threads_list.append(thread)
-
-        return thread
-
-    def run_view(self, **kwargs):
-        """ Run GUI """
-        if 'status_queue' not in kwargs:
-            raise AttributeError('Missing status_queue arg for View')
-        if 'job_queue' not in kwargs:
-            raise AttributeError('Missing job_queue arg for View')
-
-        app = self.VIEW(kwargs['status_queue'], kwargs['job_queue'])
-        app.mainloop()
-        print('bye')
-        print(self.threads_list)
+def main():
+    for i in range(5):
+        time.sleep(1)
+        print(i)
 
 if __name__ == "__main__":
-    main = main_thread()
-    main.start()
 
-    time.sleep(5)
-    main.stop()
+    app = ftp_app_view(queue.Queue(), queue.Queue())
+    test = threading.Thread(target=main)
+
+    test.start()
+    print('im here 1')
+    app.mainloop()
+    print('im here 2')
+    test.join()
+    print('im here 3')
 
